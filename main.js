07c58b8798c23a0a0ef0b87b36f28350319a7b1b
Bryan <bryanrr_julio_@hotmail.com>
2020-09-30 15:20:53 -0500

    Update main.js
    Printing of the 2 names

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ var myPerson= new Person("bryan","rodriguez");
 myPerson.printName();
 var myPerson2=new Person("juan","sanchez");
 myPerson2.printName();
+console.log(myPerson.name+" "+myPerson.lastName+" and"+myPerson2.name+" "+myPerson2.lastName);
