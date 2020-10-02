ab841707946d88ff2ee0c8fd37968272c9f06264
Bryan <bryanrr_julio_@hotmail.com>
2020-10-02 11:16:26 -0500

    Update main.js
    Printing first constant

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const moment=require('moment');
 const Person=require('./entities/Person');
+const constant1=require('FOO');
+
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 var myPerson= new Person("bryan","rodriguez");
 myPerson.printName();
 var myPerson2=new Person("juan","sanchez");
 myPerson2.printName();
 console.log(myPerson.name+" "+myPerson.lastName+" and"+myPerson2.name+" "+myPerson2.lastName);
+
+
+console.log("my first constant is: "+constant1.FOO);
